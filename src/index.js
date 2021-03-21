ee43c20553cfa7c411eb42f10a962a5a4298562a
ghoxa <hotem1234@gmail.com>
2021-03-21 18:26:18 +0900

    deleteToDo

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
-
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
+};
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id };
+};
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }]; //이전스테이트와 새로운 todo 가지고있다.
+      return [{ text: action.text, id: Date.now() }, ...state]; //이전스테이트와 새로운 todo 가지고있다.
     case DELETE_TODO:
       return [];
     default:
@@ -21,11 +26,34 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchaddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+const dispatchdeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchdeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchaddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
