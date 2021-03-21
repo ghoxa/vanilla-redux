692bb00ed2ff213f78c9af2f607b6d2eae1c78da
ghoxa <hotem1234@gmail.com>
2021-03-21 16:33:27 +0900

    todolist setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-const countmodifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
-}; //데이터를 바꾸는 함수
-
-const countstore = createStore(countmodifier);
-const onChange = () => {
-  number.innerText = countstore.getState();
 };
-countstore.subscribe(onChange);
-add.addEventListener("click", () => countstore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countstore.dispatch({ type: "MINUS" }));
-// console.log(countstore.getState()); //hello
+
+const store = createStore(reducer);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+};
+
+form.addEventListener("submit", onSubmit);
